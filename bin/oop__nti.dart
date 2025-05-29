3d9cc8874375f5762743d636bd16ccf6e9a59942
Arwa Ahmed <arwamozy@gmail.com>
2025-05-29 11:24:18 +0300

    your message

---
+++ bin/oop__nti.dart
@@ -0,0 +1,5 @@
+import 'package:oop__nti/oop__nti.dart' as oop__nti;
+
+void main(List<String> arguments) {
+
+}
---
+++ test/oop__nti_test.dart
@@ -0,0 +1,8 @@
+import 'package:oop__nti/oop__nti.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
